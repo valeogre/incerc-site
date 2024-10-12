51dca18c021f1de11904c349b3e21c7e9fabf7a9
valeogre <alex.ogrezeanu123@gmail.com>
2024-10-13 00:12:07 +0300

    nu stiu ce fac

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'Hello from the server!' });
+});
+
+// Catch all other routes and return the React app
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
